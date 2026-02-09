c17d89359dd0dff33d593ef0171161a1ed43ac1b
cdozdil <cdozdil@gmail.com>
2026-02-10 02:16:41 +0300

    Added WDDM 2.9 hook for HAGS

--- OptiScaler/hooks/Gdi32_Hooks.h
+++ OptiScaler/hooks/Gdi32_Hooks.h
@@ -9,10 +9,12 @@ static PFN_D3DKMTQueryAdapterInfo o_D3DKMTQueryAdapterInfo = nullptr;
 
 static int hkD3DKMTQueryAdapterInfo(const D3DKMT_QUERYADAPTERINFO* data)
 {
+    auto result = o_D3DKMTQueryAdapterInfo(data);
+
     // LOG_INFO("Adapter into type: {}", (uint32_t)data->Type);
     if (data->Type == KMTQAITYPE_WDDM_2_7_CAPS)
     {
-        LOG_INFO("Spoofing HAGS");
+        LOG_INFO("Spoofing HAGS 2.7");
 
         if (data->pPrivateDriverData == nullptr)
         {
@@ -24,14 +26,26 @@ static int hkD3DKMTQueryAdapterInfo(const D3DKMT_QUERYADAPTERINFO* data)
         d3dkmt_wddm_2_7_caps->HwSchSupported = 1;
         d3dkmt_wddm_2_7_caps->HwSchEnabled = 1;
         d3dkmt_wddm_2_7_caps->HwSchEnabledByDefault = 0;
-        d3dkmt_wddm_2_7_caps->IndependentVidPnVSyncControl = 0;
 
         return 0;
     }
-    else
+    else if (data->Type == KMTQAITYPE_WDDM_2_9_CAPS)
     {
-        return o_D3DKMTQueryAdapterInfo(data);
+        LOG_INFO("Spoofing HAGS 2.9");
+
+        if (data->pPrivateDriverData == nullptr)
+        {
+            LOG_ERROR("HAGS data nullptr");
+            return 0xFFFFFFFF;
+        }
+
+        auto d3dkmt_wddm_2_9_caps = static_cast<D3DKMT_WDDM_2_9_CAPS*>(data->pPrivateDriverData);
+        d3dkmt_wddm_2_9_caps->HwSchSupportState = DXGK_FEATURE_SUPPORT_STABLE;
+        d3dkmt_wddm_2_9_caps->HwSchEnabled = 1;
+        return 0;
     }
+
+    return result;
 }
 
 // Only for Linux, Wine doesn't have this function
